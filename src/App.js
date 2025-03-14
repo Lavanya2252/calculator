16c47669e147005167d623446488ea677d866f34
Lavanya2252 <lavanyaaa2252@gmail.com>
2025-03-14 10:02:29 +0530

    calculator - ui design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import "./app.css";
+import {useReducer} from "react";
+
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}){ //action -> type, payload
+
+};
 
 function App() {
+  const [{currentOperand, previousOperand, operator}, dispatch] = useReducer(reducer, {});
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          My Calculator
-        </p>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand}{operator}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>      
     </div>
   );
 }
